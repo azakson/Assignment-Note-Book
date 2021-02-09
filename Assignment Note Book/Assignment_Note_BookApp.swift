dc461bdf00225c58359ce91c4c57ce57f0ff89aa
Avery Zakson <avery.zakson@gmail.com>
2021-02-09 10:16:23 -0600

    Initial Commit

---
+++ Assignment Note Book/Assignment_Note_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_Note_BookApp.swift
+//  Assignment Note Book
+//
+//  Created by Avery Zakson on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_Note_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment Note Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment Note Book
+//
+//  Created by Avery Zakson on 2/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
