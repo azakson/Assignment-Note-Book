63174306daf7bad718d7721d82e9118c4afc1b16
Avery Zakson <avery.zakson@gmail.com>
2021-02-17 10:25:47 -0600

    Finished project and added persistent data.

--- Assignment Note Book/Assignments.swift
+++ Assignment Note Book/Assignments.swift
@@ -1,1 +1,30 @@
+//
+//  Assignments.swift
+//  Assignment Note Book
+//
+//  Created by Avery Zakson on 2/9/21.
+//
 
+import Foundation
+
+class AssignmentList: ObservableObject {
+@Published var items: [Assignment] {
+    didSet {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(items) {
+            UserDefaults.standard.set(encoded, forKey: "data")
+        }
+    }
+}
+
+init() {
+    if let items = UserDefaults.standard.data(forKey: "data") {
+        let decoder = JSONDecoder()
+        if let decoded = try? decoder.decode([Assignment].self, from: items) {
+            self.items = decoded
+            return
+        }
+    }
+    items = []
+}
+}
